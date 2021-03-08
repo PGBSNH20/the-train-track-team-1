e9b92409bd0d57a73dcc3593fd7177d4d25c34d1
Adam Brodin <worecraft@gmail.com>
2021-03-08 14:44:31 +0100

    Began implementation of ITrainPlanner & TrainSchedule

---
+++ Source/TrainEngine/TrainSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrainEngine
+{
+    class Destination
+    {
+        DateTime arrivalTime, departureTime;
+        String destinationName;
+    }
+
+    class TrainSchedule
+    {
+        List<Destination> schedule;
+    }
+}
