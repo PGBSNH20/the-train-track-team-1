41a6caabb22dbcb5d7bff2cf4b7dbd75983f848a
Albin Alm <almalbin98@gmail.com>
2021-03-10 15:02:46 +0100

    Added Track & TrackIO Class.

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -8,6 +8,15 @@ namespace TrainConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Environment.CurrentDirectory);
+            Track track = new Track();
+            TrackIO trackIO = new TrackIO();
+            track = trackIO.ParseTrack();
+            Console.WriteLine("StartLocation: " + track.StartLocation);
+            Console.WriteLine("EndLocation: " + track.EndLocation);
+            Console.WriteLine("Total Distance: " + track.TotalDistance + "KM");
+
+            /*
             TrainSchedule schedule = new TrainSchedule("Göteborg", "Stockholm", 
                 new DateTime(2021, 03, 08, 15, 30, 00), new DateTime(2021, 03, 08, 18, 30, 00), new List<Destination>()
                 {
@@ -16,9 +25,10 @@ namespace TrainConsole
                     new Destination(new DateTime(2021, 03, 08, 15, 35, 00), new DateTime(2021, 03, 08, 15, 36, 00), "Herrljunga"),
                     new Destination(new DateTime(2021, 03, 08, 15, 37, 00), new DateTime(2021, 03, 08, 15, 38, 00), "Falköping"),
                 });;
-
+    
             TrainPlanner planner = new TrainPlanner();
             planner.FollowSchedule(schedule);
+            */
         }
     }
 }
---
+++ Source/TrainEngine/TrackIO.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TrainEngine
+{
+    public class TrackIO
+    {
+        public Track ParseTrack()
+        {
+            Track track = new Track();
+            
+            StreamReader parser = new StreamReader(Environment.CurrentDirectory + @"/track.txt");
+            string input;
+            while ((input = parser.ReadLine()) != null)
+            {
+                //Ugly station-parsing!
+                string x = input;
+                string y = x;
+                x = x.Replace("-", "");
+                y = y.Replace("-", "");
+                y = y.Remove(0, 3);
+                x = x.Remove(3, 3);
+                
+                //[A]----------------------[B]
+                track.StartLocation = x;
+                track.EndLocation = y;
+                track.TotalDistance = returnDistance(input);
+            }
+            parser.Close();
+            return track;
+        }
+        private long returnDistance(string input)
+        {
+            
+            long x = 0;
+            try
+            {
+                foreach (char c in input)
+                {
+                    if (c == '-')
+                    {
+                        x += 10;
+                    }
+                }
+                return x;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+         
+            
+        }
+    }
+}
