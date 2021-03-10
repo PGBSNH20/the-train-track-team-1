f42482dba3ca083da46addec6f73c0698bf257fb
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-03-10 14:50:19 +0100

    Reformatting
    Moved validationf for trainschedule, to it's own functions.

--- Source/TrainEngine/TrainPlanner.cs
+++ Source/TrainEngine/TrainPlanner.cs
@@ -1,25 +1,10 @@
-﻿using System;
-
-namespace TrainEngine
+﻿namespace TrainEngine
 {
     public class TrainPlanner
     {
         public void FollowSchedule(TrainSchedule schedule)
         {
-            if (schedule == null)
-            {
-                throw new Exception("Schedule is null");
-            }
-            else if (String.IsNullOrEmpty(schedule.StartLocation) || String.IsNullOrWhiteSpace(schedule.StartLocation))
-            {
-                throw new Exception("Start location is null/empty");
-            }
-            else if (String.IsNullOrEmpty(schedule.EndLocation) || String.IsNullOrWhiteSpace(schedule.EndLocation))
-            {
-                throw new Exception("End location is null/empty");
-            }
-
-
+            schedule.Validate();
         }
     }
 }
--- Source/TrainEngine/TrainSchedule.cs
+++ Source/TrainEngine/TrainSchedule.cs
@@ -25,11 +25,29 @@ namespace TrainEngine
 
         public TrainSchedule(String startLocation, String endLocation, DateTime startTime, DateTime stopTime, List<Destination> destinations)
         {
-            StartLocation = startLocation;
-            EndLocation = endLocation;
+            this.StartLocation = startLocation;
+            this.EndLocation = endLocation;
             this.startTime = startTime;
             this.stopTime = stopTime;
             this.destinations = destinations;
+        }
+
+        public void Validate()
+        {
+	    if (this == null)
+            {
+                throw new Exception("Schedule is null");
+            }
+	    
+            if (String.IsNullOrEmpty(this.StartLocation) || String.IsNullOrWhiteSpace(this.StartLocation))
+            {
+                throw new Exception("Start location is null/empty");
+            }
+
+            if (String.IsNullOrEmpty(this.EndLocation) || String.IsNullOrWhiteSpace(this.EndLocation))
+            {
+                throw new Exception("End location is null/empty");
+            }
         }
     }
 }
