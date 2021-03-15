40d7f5eddb22922d6544f6861b94d870cc6d1297
Albin Alm <almalbin98@gmail.com>
2021-03-15 16:38:41 +0100

    Added several static IO methods for parsing diverse objects.

--- Source/TrainEngine/ORM/Station.cs
+++ Source/TrainEngine/ORM/Station.cs
@@ -1,9 +1,60 @@
+using System;
+using System.IO;
 namespace TrainEngine
 {
     public class Station
     {
         public string ID { get; set; }
         public long Distance { get; set; }
+
+        public static string ReturnStationName(char id)
+        {
+            StreamReader reader = new StreamReader(Environment.CurrentDirectory + "/stations.txt");
+            string input;
+            string name = "[][]"; //Weird default-value to indicate if parsing succeeded.
+            while ((input = reader.ReadLine()) != null && (!String.IsNullOrWhiteSpace(input)))
+            {
+                string[] temp = input.Split('|');
+                if (temp[1] == id.ToString())
+                {
+                    name = temp[0];
+                }
+            }
+
+            if (name == "[][]")
+            {
+                throw new Exception("Parsing failed; ID not found in stations.txt");
+            }
+            else
+            {
+                return name; 
+            }
+            
+        }
+
+        public static char ReturnStationID(string name)
+        {
+            StreamReader reader = new StreamReader(Environment.CurrentDirectory + "/stations.txt");
+            string input;
+            char id = '*';
+            while ((input = reader.ReadLine()) != null && (!String.IsNullOrWhiteSpace(input)))
+            {
+                string[] temp = input.Split('|');
+                if (temp[0] == name)
+                {
+                    id = char.Parse(temp[1]);
+                }
+            }
+
+            if (id == '*')
+            {
+                throw new Exception("Parsing failed; Name not found in stations.txt");
+            }
+            else
+            {
+                return id;
+            }
+        }
+        
     }
-    
 }
--- Source/TrainEngine/Train.cs
+++ Source/TrainEngine/Train.cs
@@ -1,4 +1,7 @@
-﻿namespace TrainEngine
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+namespace TrainEngine
 {
     public class Train
     {
@@ -13,6 +16,28 @@
             this.name = name;
             this.maxSpeedKmh = maxSpeedKmh;
             this.inUse = inUse;
+        }
+
+        public static Train[] ParseTrain(string safeFileName)
+        {
+            StreamReader reader = new StreamReader(Environment.CurrentDirectory + "/" + safeFileName);
+            List<Train> trains = new List<Train>();
+            string input;
+            while ((input = reader.ReadLine()) != null && (!String.IsNullOrWhiteSpace(input)))
+            {
+                string[] temp = input.Split('|');
+                Train train = new Train(int.Parse(temp[0]), temp[1], double.Parse(temp[2]), bool.Parse(temp[3]));
+                trains.Add(train);
+            }
+
+            if (trains.Count > 0)
+            {
+                return trains.ToArray();
+            }
+            else
+            {
+                throw new Exception("The amount of trains is parsed to 0. Is trains.txt empty?");
+            }
         }
     }
 }
